7fec098e5f8396d15e7e6a30d3c2df4eece71626
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-28 00:59:47 +0900

    kakao 검색 api 활용 / resttesttest.com에서 test결과를 Kotlin data class File from Json 플러그인을 통해서 Model 생성

---
+++ app/src/main/java/com/duran/booksearchapp/data/model/Meta.kt
@@ -0,0 +1,15 @@
+package com.duran.booksearchapp.data.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Meta(
+    @Json(name = "is_end")
+    val isEnd: Boolean,
+    @Json(name = "pageable_count")
+    val pageableCount: Int,
+    @Json(name = "total_count")
+    val totalCount: Int
+)
