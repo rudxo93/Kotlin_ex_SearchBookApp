1a0d4e21b922cb52b6fb068a276961a5eee90c9f
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-27 15:23:16 +0900

    메인 액티비티와 각 프래그먼트에 binding 적용하기

--- app/src/main/java/com/duran/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/duran/booksearchapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.duran.booksearchapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.duran.booksearchapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
